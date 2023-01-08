86753c1a86a99e3582330e6129ed56b78e25a291
Chinmay Patil <chinmaytlc@gmail.com>
2023-01-08 10:57:49 -0700

    Added android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.poketype',
+  appName: 'PokeTypeCalc',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
